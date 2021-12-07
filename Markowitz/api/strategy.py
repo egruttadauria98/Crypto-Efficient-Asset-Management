a4e65ba4f404dfb9c5a66d08bd6f23b57eaf2d77
Miyamoto Musashi <miyamoto@Miyamotos-Air.homenet.telecomitalia.it>
2021-12-07 13:36:46 +0100

    Included script in API endpoint

--- Markowitz/api/strategy.py
+++ Markowitz/api/strategy.py
@@ -1,16 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from Markowitz.portfolio_opt import portfolio_optimization
 
 @api_view(http_method_names=['GET', 'POST'])
 def markowitz(request):
     if request.method == 'GET':
         #Markowitz function
-        pass
+        coins = request.GET.getlist('coins')
+        return Response(portfolio_optimization(coins))
     elif request.method == 'POST':
         req = dict(request)
         coins = list(req.keys())
         #Markowitz function
         #Connect to DB and save results if they're not alrdy there
-        pass
-    response = {"status": 200}
-    return Response(response)
+        return Response(portfolio_optimization(request))
