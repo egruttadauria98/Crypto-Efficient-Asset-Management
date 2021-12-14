5432d79c33e8618b7d1f2b61dbdffff7926a74e4
Miyamoto Musashi <miyamoto@Miyamotos-Air.homenet.telecomitalia.it>
2021-12-14 10:50:02 +0100

    New markowitz script

--- Markowitz/portfolio_opt.py
+++ Markowitz/portfolio_opt.py
@@ -1,10 +1,12 @@
-import pandas as pd
+mport pandas as pd
 import datetime
 from finquant.portfolio import build_portfolio
 from finquant.efficient_frontier import EfficientFrontier
 
-def portfolio_optimization(list_coins='', return_rate=False, volatility=False, risk_free=0.02, start_date='2021-06-01', freq=90):
+def portfolio_optimization(list_coins, return_rate=False, volatility=False, risk_free=0.02, freq=30):
+    
     names = [s + '-USD'for s in list_coins]
+    start_date=(datetime.date.today() - datetime.timedelta(days = freq*2))
     end_date = datetime.date.today()
     pf = build_portfolio(names=names, data_api="yfinance", start_date=start_date,end_date=end_date)
     ef=EfficientFrontier(pf.comp_mean_returns(freq=1), pf.comp_cov(), risk_free_rate=risk_free, freq=freq)
@@ -14,7 +16,7 @@ def portfolio_optimization(list_coins='', return_rate=False, volatility=False, r
         max_sr=ef.efficient_return(return_rate).reset_index().rename({"index":"Crypto"},axis=1)
     if volatility!=False:
         max_sr=ef.efficient_volatility(volatility).reset_index().rename({"index":"Crypto"},axis=1)
+        
 
-    data = {max_sr.iloc[i, 0]: round(max_sr.iloc[i,1]*1000000) for i in range(max_sr.shape[0]) }
+    data = {i: {'Name':max_sr.iloc[i,0], "Allocation":round(max_sr.iloc[i,1]*1000000)} for i in range(max_sr.shape[0])} 
     return data
-
