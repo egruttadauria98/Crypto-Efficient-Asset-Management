c4e28da6cf7c602aa821b467db7d9a613e1f5b06
rodolformelo <89035960+rodolformelo@users.noreply.github.com>
2021-12-17 14:02:47 -0300

    Update Dex.sol

--- SWAP_PLAN_B/Dex.sol
+++ SWAP_PLAN_B/Dex.sol
@@ -1,17 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-
 interface IERC20 {
     function balanceOf(address account) external view returns (uint256);
     function mint(address account, uint256 amount) external;
     function burn(address account, uint256 amount) external;
 }
 
-contract SWAP {
+interface ISWAP {
+    function swaptoken(address _tokenIn, address _tokenOut, uint256 _amountIn, uint256 _amountOutMin, address _to) external;
+    function swapTokensToEther(address _address) external;
+    function swapEtherToTokens(int _shareToken1,int _shareToken2,int _shareToken3,address _address) external;
+}
 
-    AggregatorV3Interface internal priceFeed;
+contract DEX {
 
     address private Token0 = 0x8c67E632Af150673da2bEB27E63EF6189571934a; // WETH9 BOCCONI
     address private Token1 = 0xF9449a9e80Ee0f5FDFd90e8024FDf693d0502Aed; // DAI BOCCONI
@@ -20,96 +22,32 @@ contract SWAP {
     //address private Token5 = 0x8001F795da74d3E947Bf9DcD1588c75eEB500bd5;
     //address private Token6 = 0x323B18fd3352e4D4a71284aCB65EAAC40205c546;
 
-    address private PoolDaiToEther = 0x22B58f1EbEDfCA50feF632bD73368b2FdA96D541; // KOVAN ADDRESS
-    address private PoolCelerToEther = 0x1b93D8E109cfeDcBb3Cc74eD761DE286d5771511; // KOVAN ADDRESS
-    address private PoolEnjinToEther = 0xfaDbe2ee798889F02d1d39eDaD98Eff4c7fe95D4; // KOVAN ADDRESS
+    address private swapAddress= 0x309a63A1fEBF137F4E62F60b98BaBD2310204828; // Swap address contract
 
-    function _defPriceFeed(address _address) private {
-        priceFeed = AggregatorV3Interface(_address);
+    function swap(address _tokenIn, 
+                address _tokenOut, 
+                uint256 _amountIn,
+                uint256 _amountOutMin) public{
+        ISWAP(swapAddress).swaptoken(_tokenIn, _tokenOut, _amountIn, _amountOutMin, msg.sender);
     }
 
-    function swapTokensToEther(address _address) public{
-        int numberDAI = int(IERC20(Token1).balanceOf(_address));
-        int numberCELER = int(IERC20(Token2).balanceOf(_address));
-        int numberENJIN = int(IERC20(Token3).balanceOf(_address));
-        
-        // DAI Price to Ether
-        _defPriceFeed(PoolDaiToEther);
-        int priceDaiToEther = _getLatestPrice();
-
-        //CELER Price to Ether
-        _defPriceFeed(PoolCelerToEther);
-        int priceCelerToEther = _getLatestPrice();
-
-        //ENJIN Price to Ether
-        _defPriceFeed(PoolEnjinToEther);
-        int priceEnjinToEther = _getLatestPrice();
-
-        // Number of DAI
-        int numberDaiToEther = numberDAI * priceDaiToEther / (10 ** 18);
-        int numberCelerToEther = numberCELER * priceCelerToEther / (10 ** 18);
-        int numberEnjinToEther = numberENJIN * priceEnjinToEther / (10 ** 18);
-
-        // Total Ether
-        int totalEther = numberDaiToEther + numberCelerToEther + numberEnjinToEther;
-
-        // Burn DAI and USDC
-        IERC20(Token1).burn(_address,uint(numberDAI));
-        IERC20(Token2).burn(_address,uint(numberCELER));
-        IERC20(Token3).burn(_address,uint(numberENJIN));
-
-        // Mint Ether
-        IERC20(Token0).mint(_address,uint(totalEther));
-      
+    function swap1() public {
+        ISWAP(swapAddress).swapTokensToEther(msg.sender);
     }
 
-    function swapEtherToTokens(int _shareToken1,
-                                int _shareToken2,
-                                int _shareToken3,
-                                address _address) 
-                                public{
-        int numberEther = int(IERC20(Token0).balanceOf(_address));
-
-        int EtherToDai = int(_shareToken1 * numberEther / (10 ** 4));
-        int EtherToCELER = int(_shareToken2 * numberEther / (10 ** 4));
-        int EtherToENJIN = int(_shareToken3 * numberEther / (10 ** 4));
-
-        // DAI Price to Ether
-        _defPriceFeed(PoolDaiToEther);
-        int priceDaiToEther = _getLatestPrice();
-
-        //CELER Price to Ether
-        _defPriceFeed(PoolCelerToEther);
-        int priceCelerToEther = _getLatestPrice();
-
-        //ENJIN Price to Ether
-        _defPriceFeed(PoolEnjinToEther);
-        int priceEnjinToEther = _getLatestPrice();
-
-        // Number of DAI
-        int numberEtherToDai = int((EtherToDai * (10 ** 18) / priceDaiToEther));
-        int numberEtherToCELER = int((EtherToCELER * (10 ** 18) / priceCelerToEther));
-        int numberEtherToENJIN = int((EtherToENJIN * (10 ** 18) / priceEnjinToEther));
-
-        // Burn Ether
-        IERC20(Token0).burn(_address,uint(numberEther));
-
-        // Mint Tokens
-        IERC20(Token1).mint(_address,uint(numberEtherToDai));
-        IERC20(Token2).mint(_address,uint(numberEtherToCELER));
-        IERC20(Token3).mint(_address,uint(numberEtherToENJIN));
+    function swap2() public {
+        ISWAP(swapAddress).swapEtherToTokens(2000,2000,8000,msg.sender);
     }
 
-
-    function _getLatestPrice() private view returns (int) {
-        (
-            uint80 roundID, 
-            int price,
-            uint startedAt,
-            uint timeStamp,
-            uint80 answeredInRound
-        ) = priceFeed.latestRoundData();
-        return price;
+    function checkBalance(address _token) public view returns(uint256) {
+        return IERC20(_token).balanceOf(msg.sender);
     }
-      
+
+    function doMint(address _token, uint _amount) public {
+        return IERC20(_token).mint(msg.sender,_amount);
+    }    
+
+    function doBurn(address _token, uint _amount) public {
+        return IERC20(_token).burn(msg.sender,_amount);
+    }  
 }
