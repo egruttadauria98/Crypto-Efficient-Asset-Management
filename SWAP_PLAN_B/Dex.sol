05b1d59bcc5f8d9d637dce38aeefc1682882c129
rodolformelo <89035960+rodolformelo@users.noreply.github.com>
2021-12-20 14:12:26 -0300

    Update Dex.sol
    Work with fake ether + 5 different crypto

--- SWAP_PLAN_B/Dex.sol
+++ SWAP_PLAN_B/Dex.sol
@@ -8,39 +8,47 @@ interface IERC20 {
 }
 
 interface ISWAP {
-    function swaptoken(address _tokenIn, address _tokenOut, uint256 _amountIn, uint256 _amountOutMin, address _to) external;
     function swapTokensToEther(address _address) external;
-    function swapEtherToTokens(int _shareToken1,int _shareToken2,int _shareToken3,address _address) external;
+    function swapEtherToTokens(int[] memory share, address _address) external;
 }
 
 contract DEX {
 
-    address private Token0 = 0x8c67E632Af150673da2bEB27E63EF6189571934a; // WETH9 BOCCONI
-    address private Token1 = 0xF9449a9e80Ee0f5FDFd90e8024FDf693d0502Aed; // DAI BOCCONI
-    address private Token2 = 0x81F92C7FA7B76185Bae7Cd8013277B473739DD80; // CELER NETWORK BOCCONI
-    address private Token3 = 0x3E2A0e77e09Eeb4E2b7458412738cD4355c8B7B8; // ENJIN COIN
-    //address private Token5 = 0x8001F795da74d3E947Bf9DcD1588c75eEB500bd5;
-    //address private Token6 = 0x323B18fd3352e4D4a71284aCB65EAAC40205c546;
+    address[] public tokens_crypto;
 
-    address private swapAddress= 0x309a63A1fEBF137F4E62F60b98BaBD2310204828; // Swap address contract
 
-    function swap(address _tokenIn, 
-                address _tokenOut, 
-                uint256 _amountIn,
-                uint256 _amountOutMin) public{
-        ISWAP(swapAddress).swaptoken(_tokenIn, _tokenOut, _amountIn, _amountOutMin, msg.sender);
+    constructor() {
+        // DEFINING KOVAN ADRESS FOR FAKE TOKENS
+        tokens_crypto.push(0x8c67E632Af150673da2bEB27E63EF6189571934a);
+        tokens_crypto.push(0xF9449a9e80Ee0f5FDFd90e8024FDf693d0502Aed);
+        tokens_crypto.push(0x81F92C7FA7B76185Bae7Cd8013277B473739DD80);
+        tokens_crypto.push(0x3E2A0e77e09Eeb4E2b7458412738cD4355c8B7B8);
+        tokens_crypto.push(0x8001F795da74d3E947Bf9DcD1588c75eEB500bd5);
+        tokens_crypto.push(0x323B18fd3352e4D4a71284aCB65EAAC40205c546);
     }
+
+    address private swapAddress= 0x4D897B257766bBC0F4804A1D8710091D4099f88f; // Swap address contract
 
     function swap1() public {
         ISWAP(swapAddress).swapTokensToEther(msg.sender);
     }
 
     function swap2() public {
-        ISWAP(swapAddress).swapEtherToTokens(2000,2000,8000,msg.sender);
+        int[] memory share = new int[](5);
+        share[0] = 2000;
+        share[1] = 2000;
+        share[2] = 2000;
+        share[3] = 2000;
+        share[4] = 2000;
+        ISWAP(swapAddress).swapEtherToTokens(share,msg.sender);
     }
 
-    function checkBalance(address _token) public view returns(uint256) {
-        return IERC20(_token).balanceOf(msg.sender);
+    function checkBalance() public view returns(uint[] memory) {
+        uint[] memory balance = new uint[](6);
+        for (uint i = 0; i < tokens_crypto.length; i++) {
+            balance[i] = IERC20(tokens_crypto[i]).balanceOf(msg.sender);
+        }
+        return balance;
     }
 
     function doMint(address _token, uint _amount) public {
