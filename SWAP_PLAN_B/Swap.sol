f36b8cd0d60f6dd7f0dff91d49aed97a009ad57b
rodolformelo <89035960+rodolformelo@users.noreply.github.com>
2021-12-20 14:13:36 -0300

    Update Swap.sol
    Updated Swap to work with 5 different cryptos

--- SWAP_PLAN_B/Swap.sol
+++ SWAP_PLAN_B/Swap.sol
@@ -13,93 +13,117 @@ contract SWAP {
 
     AggregatorV3Interface internal priceFeed;
 
-    address private Token0 = 0x8c67E632Af150673da2bEB27E63EF6189571934a; // WETH9 BOCCONI
-    address private Token1 = 0xF9449a9e80Ee0f5FDFd90e8024FDf693d0502Aed; // DAI BOCCONI
-    address private Token2 = 0x81F92C7FA7B76185Bae7Cd8013277B473739DD80; // CELER NETWORK BOCCONI
-    address private Token3 = 0x3E2A0e77e09Eeb4E2b7458412738cD4355c8B7B8; // ENJIN COIN
-    //address private Token5 = 0x8001F795da74d3E947Bf9DcD1588c75eEB500bd5;
-    //address private Token6 = 0x323B18fd3352e4D4a71284aCB65EAAC40205c546;
+    address[] public tokens_crypto;
+    mapping (address => address) public linkPool_tokens;
 
-    address private PoolDaiToEther = 0x22B58f1EbEDfCA50feF632bD73368b2FdA96D541; // KOVAN ADDRESS
-    address private PoolCelerToEther = 0x1b93D8E109cfeDcBb3Cc74eD761DE286d5771511; // KOVAN ADDRESS
-    address private PoolEnjinToEther = 0xfaDbe2ee798889F02d1d39eDaD98Eff4c7fe95D4; // KOVAN ADDRESS
+
+    constructor() {
+        // DEFINING KOVAN ADRESS FOR FAKE TOKENS
+        tokens_crypto.push(0x8c67E632Af150673da2bEB27E63EF6189571934a); // Ether Bocconi
+        tokens_crypto.push(0xF9449a9e80Ee0f5FDFd90e8024FDf693d0502Aed); // USDT Bocconi
+        tokens_crypto.push(0x81F92C7FA7B76185Bae7Cd8013277B473739DD80); // USDC Bocconi
+        tokens_crypto.push(0x3E2A0e77e09Eeb4E2b7458412738cD4355c8B7B8); // LINK Bocconi
+        tokens_crypto.push(0x8001F795da74d3E947Bf9DcD1588c75eEB500bd5); // BTC Bocconi
+        tokens_crypto.push(0x323B18fd3352e4D4a71284aCB65EAAC40205c546); // BUSCD Bocconi
+
+        // DEFINING A MAP FOR LINK PRICE POOL BETWEEN REAL TOKENS AND ETHER
+        // https://docs.chain.link/docs/ethereum-addresses/
+        linkPool_tokens[tokens_crypto[1]] = 0x0bF499444525a23E7Bb61997539725cA2e928138; // POOL USDT/ETHER
+        linkPool_tokens[tokens_crypto[2]] = 0x64EaC61A2DFda2c3Fa04eED49AA33D021AeC8838; // POOL USDC/ETHER
+        linkPool_tokens[tokens_crypto[3]] = 0x3Af8C569ab77af5230596Acf0E8c2F9351d24C38; // POOL LINK/ETHER
+        linkPool_tokens[tokens_crypto[4]] = 0xF7904a295A029a3aBDFFB6F12755974a958C7C25; // POOL BTC/ETHER
+        linkPool_tokens[tokens_crypto[5]] = 0xbF7A18ea5DE0501f7559144e702b29c55b055CcB; // POOL BTC/ETHER
+    }
+
 
     function _defPriceFeed(address _address) private {
         priceFeed = AggregatorV3Interface(_address);
     }
 
+    //** Function to Swap the 5 different tokens in Fake Ethers, just need to insert the address
+    //** Function to Swap the 5 different tokens in Fake Ethers, just need to insert the address
+    //** Function to Swap the 5 different tokens in Fake Ethers, just need to insert the address
     function swapTokensToEther(address _address) public{
-        int numberDAI = int(IERC20(Token1).balanceOf(_address));
-        int numberCELER = int(IERC20(Token2).balanceOf(_address));
-        int numberENJIN = int(IERC20(Token3).balanceOf(_address));
+        // Creating an array of the balance of all fake tokens
+        // Since we cannot creat a dynamic map, the order fo the array must be the same of
+        // the tokens_crypto
+        int[] memory balanceToken = new int[](tokens_crypto.length);
+
+        for (uint i = 0; i < tokens_crypto.length; i++) {
+            balanceToken[i] = int(IERC20(tokens_crypto[i]).balanceOf(_address));
+        }
+
+        // Array to get the price of the real token to ether with the link pool price
+        // https://docs.chain.link/docs/ethereum-addresses/
+        int[] memory priceTokenEther = new int[](tokens_crypto.length);
+
+        for (uint i = 1; i < tokens_crypto.length; i++) {
+            _defPriceFeed(linkPool_tokens[tokens_crypto[i]]);
+            priceTokenEther[i] = _getLatestPrice();
+            
+        }
+
+        // Calculate the total amount of ether
+        // It will be the amount of tokens multiplied for the Link Pool Convertion Rate, normalized for the decimals
+        int tempEther;
+        int totalEther;
+
+        totalEther = 0;
+        for (uint i = 1; i < tokens_crypto.length; i++) {
+            tempEther = 0;
+            tempEther = balanceToken[i] * priceTokenEther[i] / (10 ** 18);
+            totalEther = totalEther + tempEther;
+        }
+
+        // Burn the total amount of each fake token
+        for (uint i = 1; i < tokens_crypto.length; i++) {
+            if (balanceToken[i] > 0) {
+                IERC20(tokens_crypto[i]).burn(_address,uint(balanceToken[i]));
+            }            
+        }
+
+        // Mint Fake Ether with the previous calculation
+        IERC20(tokens_crypto[0]).mint(_address,uint(totalEther));      
+    }
+
+    function swapEtherToTokens(int[] memory share, address _address) public{
+        // Require that the share array be the same size of all tokens but the fake ether
+
+        // Get the total amout of ether
+        int numberEther = int(IERC20(tokens_crypto[0]).balanceOf(_address));
+
+        // Number of Ether will be transferred for each token
+        int[] memory totalToken = new int[](tokens_crypto.length);
+
+        for (uint i = 1; i < tokens_crypto.length; i++) {
+            totalToken[i] = int(share[i-1] * numberEther / (10 ** 4));
+        }
+
+        //Calculate the total amount of ether
+        // It will be the amount of tokens multiplied for the Link Pool Convertion Rate, normalized for the decimals
+        int[] memory priceTokenEther = new int[](tokens_crypto.length);
+
+        for (uint i = 1; i < tokens_crypto.length; i++) {
+            _defPriceFeed(linkPool_tokens[tokens_crypto[i]]);
+            priceTokenEther[i] = _getLatestPrice();
+            
+        }
+
+        // Calculate the total amount of fake Tokens that will be transfereed
+        int[] memory calculatedBalance = new int[](tokens_crypto.length);
+
+        for (uint i = 1; i < tokens_crypto.length; i++) {
+            calculatedBalance[i] = int(totalToken[i] * (10 ** 18) /  priceTokenEther[i]);
+        }
         
-        // DAI Price to Ether
-        _defPriceFeed(PoolDaiToEther);
-        int priceDaiToEther = _getLatestPrice();
+        // Burn Fake Ether
+        IERC20(tokens_crypto[0]).burn(_address,uint(numberEther));
 
-        //CELER Price to Ether
-        _defPriceFeed(PoolCelerToEther);
-        int priceCelerToEther = _getLatestPrice();
-
-        //ENJIN Price to Ether
-        _defPriceFeed(PoolEnjinToEther);
-        int priceEnjinToEther = _getLatestPrice();
-
-        // Number of DAI
-        int numberDaiToEther = numberDAI * priceDaiToEther / (10 ** 18);
-        int numberCelerToEther = numberCELER * priceCelerToEther / (10 ** 18);
-        int numberEnjinToEther = numberENJIN * priceEnjinToEther / (10 ** 18);
-
-        // Total Ether
-        int totalEther = numberDaiToEther + numberCelerToEther + numberEnjinToEther;
-
-        // Burn DAI and USDC
-        IERC20(Token1).burn(_address,uint(numberDAI));
-        IERC20(Token2).burn(_address,uint(numberCELER));
-        IERC20(Token3).burn(_address,uint(numberENJIN));
-
-        // Mint Ether
-        IERC20(Token0).mint(_address,uint(totalEther));
-      
+        // Mint Fake Tokens
+        for (uint i = 1; i < tokens_crypto.length; i++) {
+            IERC20(tokens_crypto[i]).mint(_address,uint(calculatedBalance[i]));
+        }
     }
-
-    function swapEtherToTokens(int _shareToken1,
-                                int _shareToken2,
-                                int _shareToken3,
-                                address _address) 
-                                public{
-        int numberEther = int(IERC20(Token0).balanceOf(_address));
-
-        int EtherToDai = int(_shareToken1 * numberEther / (10 ** 4));
-        int EtherToCELER = int(_shareToken2 * numberEther / (10 ** 4));
-        int EtherToENJIN = int(_shareToken3 * numberEther / (10 ** 4));
-
-        // DAI Price to Ether
-        _defPriceFeed(PoolDaiToEther);
-        int priceDaiToEther = _getLatestPrice();
-
-        //CELER Price to Ether
-        _defPriceFeed(PoolCelerToEther);
-        int priceCelerToEther = _getLatestPrice();
-
-        //ENJIN Price to Ether
-        _defPriceFeed(PoolEnjinToEther);
-        int priceEnjinToEther = _getLatestPrice();
-
-        // Number of DAI
-        int numberEtherToDai = int((EtherToDai * (10 ** 18) / priceDaiToEther));
-        int numberEtherToCELER = int((EtherToCELER * (10 ** 18) / priceCelerToEther));
-        int numberEtherToENJIN = int((EtherToENJIN * (10 ** 18) / priceEnjinToEther));
-
-        // Burn Ether
-        IERC20(Token0).burn(_address,uint(numberEther));
-
-        // Mint Tokens
-        IERC20(Token1).mint(_address,uint(numberEtherToDai));
-        IERC20(Token2).mint(_address,uint(numberEtherToCELER));
-        IERC20(Token3).mint(_address,uint(numberEtherToENJIN));
-    }
-
 
     function _getLatestPrice() private view returns (int) {
         (
@@ -112,10 +136,4 @@ contract SWAP {
         return price;
     }
     
-    function swaptoken(address _tokenIn, address _tokenOut, uint256 _amountIn, uint256 _amountOutMin, address _to) public {
-    IERC20(_tokenIn).burn(_to, _amountIn);
-    IERC20(_tokenOut).mint(_to, _amountOutMin);
-    }
-   
 }
-    
