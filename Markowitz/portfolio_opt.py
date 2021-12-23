37032201ed1863b18f356fb0e468615f8f7b97e1
applebar17 <84227920+applebar17@users.noreply.github.com>
2021-12-23 18:29:04 +0100

    Update portfolio_opt.py

--- Markowitz/portfolio_opt.py
+++ Markowitz/portfolio_opt.py
@@ -5,12 +5,19 @@ from finquant.efficient_frontier import EfficientFrontier
 
 def portfolio_optimization(list_coins, return_rate=False, volatility=False, risk_free=0.0232, freq=30):
     
-    names = [s + '-USD'for s in list_coins]
+    names = [coin + '-USD'for coin in list_coins]
     start_date=(datetime.date.today() - datetime.timedelta(days = freq*3))
     end_date = datetime.date.today()
     pf = build_portfolio(names=names, data_api="yfinance", start_date=start_date,end_date=end_date)
     pf.data.dropna(axis=1,how='all',inplace=True)
-    ef=EfficientFrontier(pf.comp_mean_returns(freq=1), pf.comp_cov(), risk_free_rate=risk_free, freq=freq)
+    pf.data=pf.data.fillna('nan')
+    for i in range(pf.data.shape[0]):
+        for k in range(pf.data.shape[1]):
+            if pf.data.iloc[i,k]=='nan':
+                pf.data.iloc[i,k]=pf.data.iloc[i-1,k]
+    pf.freq=freq
+    pf.risk_free_rate=risk_free
+    ef=EfficientFrontier(pf.comp_mean_returns(freq=freq*3), pf.comp_cov(), risk_free_rate=risk_free, freq=freq)
     if return_rate==False and volatility==False:
         max_sr=ef.maximum_sharpe_ratio().reset_index().rename({"index":"Crypto"},axis=1)
     if return_rate!=False:
